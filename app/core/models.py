2bc634770e227cf8350a1e9db9cd3fe4813b460b
Fang Liu <fangyiliu@gmail.com>
2022-03-20 21:32:20 -0700

    Correct typo

--- app/core/models.py
+++ app/core/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extrar_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email), **extrar_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
