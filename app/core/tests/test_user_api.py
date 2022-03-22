3989caf688cb6e5761d21c79fc2bae7d844ada3d
Fang Liu <fangyiliu@gmail.com>
2022-03-22 16:12:11 -0700

    Fix user tests

--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -36,7 +36,11 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload = {'email': 'test@fang.com', 'password': 'testpass'}
+        payload = {
+            'email': 'test@fang.com',
+            'password': 'testpass',
+            'name': 'Test',
+        }
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -45,7 +49,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 charcters"""
-        payload = {'email': 'test@fang.com', 'password': 'pw'}
+        payload = {
+            'email': 'test@fang.com',
+            'password': 'pw',
+            'name': 'Test',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
