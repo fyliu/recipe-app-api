e3b09da6ad59e1b1017857cf83ee8bfa7927ff87
Fang Liu <fyliu@users.noreply.github.com>
2022-03-25 15:20:00 -0700

    Add feature to list ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -62,5 +62,3 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
-
-
